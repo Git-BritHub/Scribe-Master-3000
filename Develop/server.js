8b015cc937b56251b8221f61bdada85c370f41cc
Brittany Brimley <brittanysapp@gmail.com>
2023-08-27 21:51:56 -0600

    created server.js and added routes for html pages

---
+++ Develop/server.js
@@ -0,0 +1,20 @@
+// Variables of packages needed to be imported
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// Port the Express.js server will run
+const PORT = 3001;
+
+// Static middleware pointing to the public folder
+app.use(express.static("public"));
+
+// Express.js routes for default "/", "/notes" and "/index" endpoints
+app.get("/", (req, res) => res.send("Navigate to /notes or/index"));
+
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
+
+app.get("/index", (req, res) => res.sendFile(path.join(__dirname, "public/index.html")));
+
+// listening for incoming connections on specified port
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));

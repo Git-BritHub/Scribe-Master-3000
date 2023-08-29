acccba4f3924f98fbbb3269afae55f61c7dc2e18
Brittany Brimley <brittanysapp@gmail.com>
2023-08-29 16:01:02 -0600

    added API route for db.json

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const noteData = require("./db/db.json")
+// const fs = require("fs");
 
 // Port the Express.js server will run
 const PORT = 3001;
@@ -15,6 +17,9 @@ app.get("/", (req, res) => res.send("Navigate to /notes or/index"));
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
 
 app.get("/index", (req, res) => res.sendFile(path.join(__dirname, "public/index.html")));
+
+// API route for db.json
+app.get("/api/notes", (req, res) => res.json(noteData));
 
 // listening for incoming connections on specified port
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));

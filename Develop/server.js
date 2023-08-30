0af9529d54aca089c6310d0d05bc5274c4005888
Brittany Brimley <brittanysapp@gmail.com>
2023-08-29 22:07:42 -0600

    created fsUtils.js file in helpers folder and edited app.post in server.js so that new notes append to db file

--- Develop/server.js
+++ Develop/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const uuid = require("./helpers/uuid");
 const noteData = require("./db/db.json");
+const { readAndAppend } = require("./helpers/fsUtils");
 const app = express();
 // const fs = require("fs");
 
@@ -10,10 +11,12 @@ const app = express();
 const PORT = 3001;
 
 // Static middleware pointing to the public folder
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Express.js routes for default "/", "/notes" and "/index" endpoints
-app.get("/", (req, res) => res.send("Navigate to /notes or/index"));
+// app.get("/", (req, res) => res.send("Navigate to /notes or/index"));
 
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
 
@@ -24,6 +27,7 @@ app.get("/api/notes", (req, res) => res.json(noteData));
 
 // POST request to add new note with note ID
 app.post("/api/notes", (req, res) => {
+    console.log(req.body);
     const { title, text } = req.body;
     if (title && text) {
         const newNote = {
@@ -36,6 +40,8 @@ app.post("/api/notes", (req, res) => {
             status: "success",
             body: newNote,
         };
+
+        readAndAppend(newNote, "./db/db.json");
         
         console.log(response);
         res.status().json(response);
